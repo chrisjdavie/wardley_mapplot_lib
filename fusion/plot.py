4837874923c047b9ce8236c82f1ec76676ca5aca
Chris Davie <cjdavie@googlemail.com>
2021-06-28 17:30:28 +0100

    add radioactive waste management to fusion

--- fusion/plot.py
+++ fusion/plot.py
@@ -6,19 +6,16 @@ import matplotlib.patches as mpatches
 
 from plot_map import draw_wardley_map_from_json
 
-print(getcwd())
-
-
 data_dir = Path("fusion")
 graph_path = data_dir / "data.json"
 draw_wardley_map_from_json(graph_path)
 
-rect = mpatches.Rectangle(
-    (0, 0.0), 1.2, 0.49,
-    color="plum",
-    zorder=-2
-)
-plt.gca().add_patch(rect)
+# rect = mpatches.Rectangle(
+#     (0, 0.0), 1.2, 0.49,
+#     color="plum",
+#     zorder=-2
+# )
+# plt.gca().add_patch(rect)
 
 image_path = data_dir / "automated.svg"
 plt.savefig(image_path)
@@ -26,4 +23,7 @@ plt.savefig(image_path)
 plt.show()
 
 # TODO
-# Add text for the purple box
+# Add key
+# o - node
+# - - dependency
+# purple - will need
